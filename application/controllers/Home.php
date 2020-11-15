e15c10a8fb2c1b502592d48e44ffb46a177bd93a
Shifa Mulla <kshifa0394@gmail.com>
2020-11-15 23:03:29 +0530

    Modified home.php and added readme.txt

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -18,6 +18,7 @@ class Home extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+	
 	public function index()
 	{
 		$this->load->view('blog_list_page');
